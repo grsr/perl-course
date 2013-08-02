11447404e1a2e42952df9132b93550cf36da0045
John Davey <johnomics@gmail.com>
2013-08-02 23:53:13 +0100

    Cleaning up folder structure

--- perlcourse/central_dogma.pl
+++
@@ -1,5 +0,0 @@
-# central_dogma.pl
-# Print the Central Dogma of Molecular Biology
-use strict;
-use warnings;
-print "Once information has passed into protein it cannot get out again\n"; # See Crick 1970
